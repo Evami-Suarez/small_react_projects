2b709b792ba427332e500356d1ebd7fbfb78ab98
Evami-Suarez <suarezevami20@gmail.com>
2025-11-12 01:08:50 +0800

    Done with accordian small project

---
+++ small_projects/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Accordian from "./components/accordian/index.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <Accordian />
+    </div>
+  );
+};
+
+export default App;
