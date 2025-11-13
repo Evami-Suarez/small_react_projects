0af72db5a11f26a17df7c077d297619ea85f0aa5
Evami-Suarez <suarezevami20@gmail.com>
2025-11-13 22:27:14 +0800

    done with star-rating

--- small_projects/src/App.jsx
+++ small_projects/src/App.jsx
@@ -1,13 +1,16 @@
 //import "./App.css";
 import Accordian from "./components/accordian/index.jsx";
 import RandomColor from "./components/random-color/index.jsx";
+import StarRating from "./components/star-rating/index.jsx";
 
 const App = () => {
   return (
     <div>
       {/* <Accordian /> */}
 
-      <RandomColor />
+      {/* <RandomColor /> */}
+
+      <StarRating noOfStars={5} />
     </div>
   );
 };
